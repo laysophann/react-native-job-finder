a86f5158b42c2c236f3593a7b37d210af4152697
laysophann <lay.sophann19@kit.edu.kh>
2023-09-04 23:57:53 +0700

    home screen

---
+++ hook/useFetch.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+}
